1a4897521e998cf2acb0344ee71491fcafd79ed5
Dhanush Gopinath <dhanush@beyondbytes.co.in>
2014-02-20 13:07:04 +0530

    Removed Hikari CP and added Basic DS

--- src/main/java/com/cloudezz/houston/config/DatabaseConfiguration.java
+++ src/main/java/com/cloudezz/houston/config/DatabaseConfiguration.java
@@ -1,8 +1,15 @@
 package com.cloudezz.houston.config;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
 import liquibase.integration.spring.SpringLiquibase;
+
+import org.apache.commons.dbcp.BasicDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.bind.RelaxedPropertyResolver;
@@ -14,67 +21,67 @@ import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import javax.inject.Inject;
-import javax.sql.DataSource;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 @Configuration
 @EnableJpaRepositories("com.cloudezz.houston.repository")
 @EnableTransactionManagement
 public class DatabaseConfiguration implements EnvironmentAware {
 
-    private final Logger log = LoggerFactory.getLogger(DatabaseConfiguration.class);
+	private final Logger log = LoggerFactory
+			.getLogger(DatabaseConfiguration.class);
 
-    private RelaxedPropertyResolver propertyResolver;
+	private RelaxedPropertyResolver propertyResolver;
 
-    @Inject
-    private Environment env;
+	@Inject
+	private Environment env;
 
-    @Override
-    public void setEnvironment(Environment environment) {
-        this.propertyResolver = new RelaxedPropertyResolver(environment, "spring.datasource.");
-    }
+	@Override
+	public void setEnvironment(Environment environment) {
+		this.propertyResolver = new RelaxedPropertyResolver(environment,
+				"spring.datasource.");
+	}
 
-    @Bean
-    public DataSource dataSource() {
-        log.debug("Configuring Datasource");
-        if (propertyResolver.getProperty("url") == null && propertyResolver.getProperty("databaseName") == null) {
-            log.error("Your database connection pool configuration is incorrect! The application" +
-                    "cannot start. Please check your Spring profile, current profiles are: {}",
-                    Arrays.toString(env.getActiveProfiles()));
+	@Bean
+	public DataSource dataSource() {
+		log.debug("Configuring Datasource");
+		if (propertyResolver.getProperty("url") == null
+				&& propertyResolver.getProperty("databaseName") == null) {
+			log.error(
+					"Your database connection pool configuration is incorrect! The application"
+							+ "cannot start. Please check your Spring profile, current profiles are: {}",
+					Arrays.toString(env.getActiveProfiles()));
 
-            throw new ApplicationContextException("Database connection pool is not configured correctly");
-        }
-        HikariConfig config = new HikariConfig();
-        config.setDataSourceClassName(propertyResolver.getProperty("dataSourceClassName"));
-        if (propertyResolver.getProperty("url") == null || "".equals(propertyResolver.getProperty("url"))) {
-            config.addDataSourceProperty("databaseName", propertyResolver.getProperty("databaseName"));
-            config.addDataSourceProperty("serverName", propertyResolver.getProperty("serverName"));
-        } else {
-            config.addDataSourceProperty("url", propertyResolver.getProperty("url"));
-        }
-        config.addDataSourceProperty("user", propertyResolver.getProperty("username"));
-        config.addDataSourceProperty("password", propertyResolver.getProperty("password"));
-        return new HikariDataSource(config);
-    }
+			throw new ApplicationContextException(
+					"Database connection pool is not configured correctly");
+		}
 
-    @Bean(name = {"org.springframework.boot.autoconfigure.AutoConfigurationUtils.basePackages"})
-    public List<String> getBasePackages() {
-        List<String> basePackages = new ArrayList<>();
-        basePackages.add("com.cloudezz.houston.domain");
-        return basePackages;
-    }
+		BasicDataSource dataSource = new BasicDataSource();
 
-    @Bean
-    public SpringLiquibase liquibase() {
-        log.debug("Configuring Liquibase");
-        SpringLiquibase liquibase = new SpringLiquibase();
-        liquibase.setDataSource(dataSource());
-        liquibase.setChangeLog("classpath:config/liquibase/db-changelog.xml");
-        liquibase.setContexts("development, production");
-        return liquibase;
-    }
+		dataSource.setDriverClassName(propertyResolver
+				.getProperty("driverClass"));
+		if (propertyResolver.getProperty("url") == null
+				|| "".equals(propertyResolver.getProperty("url"))) {
+		} else {
+			dataSource.setUrl(propertyResolver.getProperty("url"));
+		}
+		dataSource.setUsername(propertyResolver.getProperty("username"));
+		dataSource.setPassword(propertyResolver.getProperty("password"));
+		return dataSource;
+	}
+
+	@Bean(name = { "org.springframework.boot.autoconfigure.AutoConfigurationUtils.basePackages" })
+	public List<String> getBasePackages() {
+		List<String> basePackages = new ArrayList<>();
+		basePackages.add("com.cloudezz.houston.domain");
+		return basePackages;
+	}
+
+	@Bean
+	public SpringLiquibase liquibase() {
+		log.debug("Configuring Liquibase");
+		SpringLiquibase liquibase = new SpringLiquibase();
+		liquibase.setDataSource(dataSource());
+		liquibase.setChangeLog("classpath:config/liquibase/db-changelog.xml");
+		liquibase.setContexts("development, production");
+		return liquibase;
+	}
 }
-
