ef3c1dc2311859e4beb21fce0c5b567da13db307
tanmally <tm@beyondbytes.co.in>
2014-03-02 21:51:41 +0530

    Added env form java pojos

---
+++ src/main/java/com/cloudezz/houston/domain/EnvFormElement.java
@@ -0,0 +1,123 @@
+package com.cloudezz.houston.domain;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.XmlValue;
+
+/**
+ * <p>
+ * Java class for anonymous complex type form element.
+ * 
+ * <p>
+ * The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;simpleContent>
+ *     &lt;extension base="&lt;http://www.w3.org/2001/XMLSchema>string">
+ *       &lt;attribute name="type" type="{http://www.w3.org/2001/XMLSchema}string" />
+ *       &lt;attribute name="name" type="{http://www.w3.org/2001/XMLSchema}string" />
+ *       &lt;attribute name="display-name" type="{http://www.w3.org/2001/XMLSchema}string" />
+ *     &lt;/extension>
+ *   &lt;/simpleContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {"value"})
+public class EnvFormElement {
+
+  @XmlValue
+  protected String value;
+  @XmlAttribute(name = "type")
+  protected String type;
+  @XmlAttribute(name = "name")
+  protected String name;
+  @XmlAttribute(name = "display-name")
+  protected String displayName;
+
+  /**
+   * Gets the value of the value property.
+   * 
+   * @return possible object is {@link String }
+   * 
+   */
+  public String getValue() {
+    return value;
+  }
+
+  /**
+   * Sets the value of the value property.
+   * 
+   * @param value allowed object is {@link String }
+   * 
+   */
+  public void setValue(String value) {
+    this.value = value;
+  }
+
+  /**
+   * Gets the value of the type property.
+   * 
+   * @return possible object is {@link String }
+   * 
+   */
+  public String getType() {
+    return type;
+  }
+
+  /**
+   * Sets the value of the type property.
+   * 
+   * @param value allowed object is {@link String }
+   * 
+   */
+  public void setType(String value) {
+    this.type = value;
+  }
+
+  /**
+   * Gets the value of the name property.
+   * 
+   * @return possible object is {@link String }
+   * 
+   */
+  public String getName() {
+    return name;
+  }
+
+  /**
+   * Sets the value of the name property.
+   * 
+   * @param value allowed object is {@link String }
+   * 
+   */
+  public void setName(String value) {
+    this.name = value;
+  }
+
+  /**
+   * Gets the value of the displayName property.
+   * 
+   * @return possible object is {@link String }
+   * 
+   */
+  public String getDisplayName() {
+    return displayName;
+  }
+
+  /**
+   * Sets the value of the displayName property.
+   * 
+   * @param value allowed object is {@link String }
+   * 
+   */
+  public void setDisplayName(String value) {
+    this.displayName = value;
+  }
+
+}
