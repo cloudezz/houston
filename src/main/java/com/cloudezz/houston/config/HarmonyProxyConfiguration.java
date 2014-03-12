96cf5bf268ac0f8ae526f475c1ec8437cccdb623
tanmally <tm@beyondbytes.co.in>
2014-03-13 01:26:12 +0530

    Added haproxy management from cloudezz

---
+++ src/main/java/com/cloudezz/houston/config/HarmonyProxyConfiguration.java
@@ -0,0 +1,35 @@
+package com.cloudezz.houston.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.cloudezz.harmony.HarmonyConfigurator;
+
+@Configuration
+public class HarmonyProxyConfiguration {
+
+  private final Logger log = LoggerFactory.getLogger(HarmonyProxyConfiguration.class);
+
+
+  @Bean
+  public HarmonyConfigurator harmonyProxyClient() throws Exception {
+
+    String urlConfigFile =
+        this.getClass().getResource("/harmony/HAProxyConfigurator.properties").getPath();
+    HarmonyConfigurator harmonyConfigurator = new HarmonyConfigurator(urlConfigFile);
+
+
+    // check reverse proxy server conn settings
+    if (!harmonyConfigurator.proxyConnectionStatus()) {
+      log.warn("Warning! Not able to reach harmony reverse proxy server");
+      log.debug("Did you configure your harmony HAproxy server settings in your harmony/HAProxyConfigurator.properties file");
+    }
+
+    return harmonyConfigurator;
+  }
+
+
+
+}
