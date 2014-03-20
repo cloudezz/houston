3122420a3e48aa20d95057a11dc4e471a96f25ee
tanmally <tm@beyondbytes.co.in>
2014-03-21 03:25:23 +0530

    added serial id

--- src/main/java/com/cloudezz/houston/web/servlet/HealthCheckServlet.java
+++ src/main/java/com/cloudezz/houston/web/servlet/HealthCheckServlet.java
@@ -8,16 +8,22 @@ import javax.servlet.http.HttpServletResponse;
 
 public class HealthCheckServlet extends com.codahale.metrics.servlets.HealthCheckServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
 
-        // Overrides the default behaviour. We don't want to return 500 Internal Service Error if one or more fail.
-        // This count has an error 500, which is not the case.
-        //
-        // HealthCheckServlet responds to GET requests by running all the [health checks](#health-checks)
-        // and returning 501 Not Implemented if no health checks are registered, 200 OK if all pass,
-        // or 500 Internal Service Error if one or more fail.
-        resp.setStatus(HttpServletResponse.SC_OK);
-    }
+  private static final long serialVersionUID = 5261405701328494624L;
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
+      IOException {
+    super.doGet(req, resp);
+
+    // Overrides the default behaviour. We don't want to return 500 Internal Service Error if one or
+    // more fail.
+    // This count has an error 500, which is not the case.
+    //
+    // HealthCheckServlet responds to GET requests by running all the [health
+    // checks](#health-checks)
+    // and returning 501 Not Implemented if no health checks are registered, 200 OK if all pass,
+    // or 500 Internal Service Error if one or more fail.
+    resp.setStatus(HttpServletResponse.SC_OK);
+  }
 }
